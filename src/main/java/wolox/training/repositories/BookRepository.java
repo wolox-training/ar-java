0ebaa256370e09b62db8a96b58146bac9384bc25
Alan Rinaldi <alan.rinaldi@wolox.com.ar>
2020-05-18 10:47:15 -0300

    Fixed book repository with the correct id

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -3,6 +3,6 @@ package wolox.training.repositories;
 import org.springframework.data.repository.Repository;
 import wolox.training.models.Book;
 
-public interface BookRepository extends Repository<Book, String> {
+public interface BookRepository extends Repository<Book, Long> {
     Book findFirstByAuthor (String author);
 }
