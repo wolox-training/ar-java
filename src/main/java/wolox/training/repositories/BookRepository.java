767a27aafee8f892a0083580175386b17086af69
Alan Rinaldi <alan.rinaldi@wolox.com.ar>
2020-05-22 12:10:09 -0300

    Deleted unnecessary methods in BookRepository

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -1,18 +1,15 @@
 package wolox.training.repositories;
 
-import java.util.List;
 import java.util.Optional;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import wolox.training.models.Book;
 
-public interface BookRepository extends Repository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
     Optional<Book> findFirstByAuthor (final String author);
 
     Optional<Book> findById(final long id);
 
-    List<Book> findAll();
+//    void deleteById(final long id);
 
-    void deleteById(final long id);
-
-    Book save(final Book book);
+//    Book save(final Book book);
 }
